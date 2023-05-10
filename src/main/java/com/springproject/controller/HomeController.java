f1ec2fa6a86f9055a37f39df9ed2d4e6410bc481
rilac <eodos6480@gmail.com>
2023-05-10 15:28:36 +0900

    test로 바꿔봤습니다

--- src/main/java/com/springproject/controller/HomeController.java
+++ src/main/java/com/springproject/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.springproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -31,4 +32,7 @@ public class HomeController {
     public String about(){
         return "about";
     }
+
+    @PostMapping
+    public String aboutc(){ return "test";}
 }
