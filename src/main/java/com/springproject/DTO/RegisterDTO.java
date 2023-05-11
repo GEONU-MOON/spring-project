cb075d27a61a939c26e5a3f022aa0240396004ef
rilac <eodos6480@gmail.com>
2023-05-11 15:39:01 +0900

    index form 태그 삽입, DTO 추가, 연동 작업중

---
+++ src/main/java/com/springproject/DTO/RegisterDTO.java
@@ -0,0 +1,8 @@
+package com.springproject.DTO;
+
+public class RegisterDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+}
--- src/main/java/com/springproject/controller/HomeController.java
+++ src/main/java/com/springproject/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.springproject.controller;
 
+import com.springproject.DTO.RegisterDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model) {
+        model.addAttribute("registerform", new RegisterDTO());
         return "index";
     }
 
@@ -39,5 +42,14 @@ public class HomeController {
     @PostMapping("/home")
     public String Login() {
         return "home";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestParam("registerId")String Id,
+                           @RequestParam("registerName")String name,
+                           @RequestParam("registerEmail")String email,
+                           @RequestParam("registerPassword")String password){
+
+        return "redirect:/";
     }
 }
