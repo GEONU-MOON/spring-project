022b4ab6a7b4050244684000a5115bb031943985
rilac <eodos6480@gmail.com>
2023-05-13 13:01:12 +0900

    테스터데이터에 게시물 추가 중(미완)

--- src/main/java/com/springproject/TestDataInit.java
+++ src/main/java/com/springproject/TestDataInit.java
@@ -1,5 +1,6 @@
 package com.springproject;
 
+import com.springproject.domain.Board;
 import com.springproject.domain.Members;
 import com.springproject.repository.BoardRepository;
 import com.springproject.repository.MembersRepository;
@@ -27,4 +28,24 @@ public class TestDataInit {
 
         memberRepository.save(member);
     }
+//    @PostConstruct
+//    @Transactional
+//    public void init2(){
+//        Members member = memberRepository.findByID("test");
+//        Board board = new Board();
+//        board.setTitle("테스트 데이터 제목");
+//        board.setContent("테스트 데이터 컨텐트");
+//        board.setMember(member);
+//        board.setThumbnail("썸네일 경로1");
+//
+//        Board board2 = new Board();
+//        board.setTitle("테스트 데이터 제목2");
+//        board.setContent("테스트 데이터 컨텐트2");
+//        board.setMember(member);
+//        board.setThumbnail("썸네일 경로2");
+//
+//        boardRepository.save(board);
+//        boardRepository.save(board2);
+//    }
+
 }
