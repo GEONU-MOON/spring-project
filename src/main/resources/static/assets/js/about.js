239d20ba4f26375616894d43e32d17afafcfcec4
GEONU-MOON <moondy2209@naver.com>
2023-05-15 15:45:55 +0900

    회원정보 수정 / 탈퇴 모든 기능 구현 완료!

--- src/main/resources/static/assets/js/about.js
+++ src/main/resources/static/assets/js/about.js
@@ -19,10 +19,55 @@ function checkPassword(action) {
     }
 }
 
+function validateInput() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const newPassword = document.getElementById("newpassword").value;
+    const newPasswordCk = document.getElementById("newpasswordck").value;
+
+    const namePattern = /^[가-힣a-zA-Z]+$/;
+    const newPasswordPattern = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{4,12}$/;
+
+    if (!namePattern.test(name)) {
+        alert("이름은 한글, 영어만 입력할 수 있습니다.");
+        return false;
+    }
+
+    if (!validateEmail(email)) {
+        alert("유효한 이메일 주소를 입력해주세요.");
+        return false;
+    }
+
+    if (!newPassword || !newPasswordCk) {
+        alert("모든 입력란을 채워주세요.");
+        return false;
+    }
+
+    if (!newPasswordPattern.test(newPassword)) {
+        alert("새 비밀번호는 영어, 숫자, 특수문자를 섞어 4~12자리로 만들어야 합니다.");
+        return false;
+    }
+
+    if (newPassword !== newPasswordCk) {
+        alert("새 비밀번호와 새 비밀번호 확인이 일치하지 않습니다.");
+        return false;
+    }
+
+    return true;
+}
+
+function validateEmail(email) {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+}
+
 document.getElementById("update-btn").addEventListener("click", function () {
-    checkPassword("update");
+    if (validateInput()) {
+        checkPassword("update");
+    }
 });
 
 document.getElementById("delete-btn").addEventListener("click", function () {
     checkPassword("delete");
 });
+
