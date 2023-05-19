2e565682495a0c337873edb53dff7eea3755c7b2
GEONU-MOON <moondy2209@naver.com>
2023-05-19 13:17:08 +0900

    댓글 기능 구현중..~ CommentDTO수정 blogpost.html 수정

--- src/main/java/com/springproject/DTO/CommentDTO.java
+++ src/main/java/com/springproject/DTO/CommentDTO.java
@@ -2,15 +2,21 @@ package com.springproject.DTO;
 
 import com.springproject.domain.Board;
 import com.springproject.domain.Members;
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDate;
 
+@Getter
+@Setter
 public class CommentDTO {
 
-    private Board board;
+    private Long boardId;
 
-    private Members member;
+    private Long memberId;
 
     private String content;
 
     private LocalDate postDate;
+
 }
+
